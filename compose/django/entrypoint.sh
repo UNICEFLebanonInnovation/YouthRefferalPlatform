e960b6a26bb085438758d79e5dc41fb4f8800bfa
achamseddine <achamseddine@unicef.org>
2018-01-12 17:42:36 +0200

    Collect static files

--- compose/django/entrypoint.sh
+++ compose/django/entrypoint.sh
@@ -11,6 +11,10 @@ if [ -z "$POSTGRES_USER" ]; then
     export POSTGRES_USER=postgres
 fi
 
+if [ "x$DJANGO_COLLECTSTATIC" = 'xon' ]; then
+    python manage.py collectstatic --noinput
+fi
+
 export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_USER
 
 export CELERY_BROKER_URL=$REDIS_URL/0
