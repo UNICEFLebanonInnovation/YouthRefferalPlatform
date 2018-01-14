83c21b093ebbf46a00ffc2a8a64e2b8414a457e7
Tarek Moubarak <tmoubarak@fusionsecond.com>
2018-01-14 15:23:27 +0200

    Search Filter fix

--- referral_platform/registrations/filters.py
+++ referral_platform/registrations/filters.py
@@ -9,7 +9,7 @@ from .models import Registration
 
 class CommonFilter(FilterSet):
 
-    governorate = ModelChoiceFilter(queryset=Location.objects.filter(parent__isnull=False),
+    registration__governorate = ModelChoiceFilter(queryset=Location.objects.filter(parent__isnull=False),
                                     empty_label=_('Governorate'))
 
     # nationality = ModelChoiceFilter(queryset=Nationality.objects.all(), empty_label=_('Nationality'))
@@ -20,22 +20,22 @@ class YouthFilter(CommonFilter):
     class Meta:
         model = Registration
         fields = {
-            'youth__bayanati_ID': ['contains'],
-            'location': ['contains'],
-            'trainer': ['contains'],
-            'youth__first_name': ['contains'],
-            'youth__father_name': ['contains'],
-            'youth__last_name': ['contains'],
+            'registration__youth__bayanati_ID': ['contains'],
+            'registration__location': ['contains'],
+            'registration__trainer': ['contains'],
+            'registration__youth__first_name': ['contains'],
+            'registration__youth__father_name': ['contains'],
+            'registration__youth__last_name': ['contains'],
             # 'nationality': ['exact'],
-            'governorate': ['exact'],
-            'youth__marital_status': ['exact'],
-            'youth__sex': ['exact'],
+            'registration__governorate': ['exact'],
+            'registration__youth__marital_status': ['exact'],
+            'registration__youth__sex': ['exact'],
         }
 
 
 class YouthPLFilter(FilterSet):
 
-    governorate = ModelChoiceFilter(queryset=Location.objects.filter(parent__p_code="PALESTINE"),
+    registration__governorate = ModelChoiceFilter(queryset=Location.objects.filter(parent__p_code="PALESTINE"),
                                     empty_label=_('Governorate'))
 
     # nationality = ModelChoiceFilter(queryset=Nationality.objects.all(), empty_label=_('Nationality'))
@@ -43,20 +43,20 @@ class YouthPLFilter(FilterSet):
     class Meta:
         model = Registration
         fields = {
-            'location': ['contains'],
-            'youth__first_name': ['contains'],
-            'youth__father_name': ['contains'],
-            'youth__last_name': ['contains'],
+            'registration__location': ['contains'],
+            'registration__youth__first_name': ['contains'],
+            'registration__youth__father_name': ['contains'],
+            'registration__youth__last_name': ['contains'],
             # 'youth__nationality': ['exact'],
-            'governorate': ['exact'],
+            'registration__governorate': ['exact'],
             'youth__marital_status': ['exact'],
             'youth__sex': ['exact'],
         }
 
 
 class YouthSYFilter(FilterSet):
-
-    governorate = ModelChoiceFilter(queryset=Location.objects.filter(parent__p_code="SYRIA"),
+    
+    registration__governorate = ModelChoiceFilter(queryset=Location.objects.filter(parent__p_code="SYRIA"),
                                     empty_label=_('Governorate'))
 
     # nationality = ModelChoiceFilter(queryset=Nationality.objects.all(), empty_label=_('Nationality'))
@@ -64,12 +64,12 @@ class YouthSYFilter(FilterSet):
     class Meta:
         model = Registration
         fields = {
-            'location': ['contains'],
-            'youth__first_name': ['contains'],
-            'youth__father_name': ['contains'],
-            'youth__last_name': ['contains'],
+            'registration__location': ['contains'],
+            'registration__youth__first_name': ['contains'],
+            'registration__youth__father_name': ['contains'],
+            'registration__youth__last_name': ['contains'],
             # 'youth__nationality': ['exact'],
-            'governorate': ['exact'],
-            'youth__marital_status': ['exact'],
-            'youth__sex': ['exact'],
+            'registration__governorate': ['exact'],
+            'registration__youth__marital_status': ['exact'],
+            'registration__youth__sex': ['exact'],
         }
