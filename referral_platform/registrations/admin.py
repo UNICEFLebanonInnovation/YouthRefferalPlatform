c139a88fe15bdc27b4d7d4f80d27441aefa98534
Rami Sweidane <rami.sweidane@gmail.com>
2018-07-30 12:44:47 +0300

    Added partner as a filter in admin

--- referral_platform/registrations/admin.py
+++ referral_platform/registrations/admin.py
@@ -237,6 +237,7 @@ class RegistrationAdmin(ImportExportModelAdmin):
         'youth__sex',
         'youth__nationality',
         'youth__marital_status',
+        'partner_organization',
         'created',
         'modified',
     )
