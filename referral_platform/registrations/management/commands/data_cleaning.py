8f8b8551d7d0f87497a6ec2118133b4b473d318b
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-10-17 12:28:38 +0300

    test

--- referral_platform/registrations/management/commands/data_cleaning.py
+++ referral_platform/registrations/management/commands/data_cleaning.py
@@ -8,10 +8,19 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--filename', type=str, default=None)
+        parser.add_argument('--updated', type=str, default=None)
 
     def handle(self, *args, **options):
-        filename = options['filename']
+        filename = options['filename'],
+        updated = options['updated'],
+        # # help = 'use all to update all, or new to update only empty new data'
+        # if filename:
+        #     from referral_platform.registrations.models import AssessmentSubmission
+        #     newmaping = AssessmentSubmission.objects.filter(assessment__slug=filename)
+        #     for obj in newmaping:
+        #         obj.update_field()
+        # else:
         from referral_platform.registrations.models import AssessmentSubmission
-        newmaping = AssessmentSubmission.objects.filter(assessment__slug=filename)
+        newmaping = AssessmentSubmission.objects.filter(registration__partner_organization='Nour')
         for obj in newmaping:
             obj.update_field()
