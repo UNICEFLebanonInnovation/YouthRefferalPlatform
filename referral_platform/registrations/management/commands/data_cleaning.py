606e15c31c30e43ee64fea6899399b4de13b2e9a
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-10-17 10:32:21 +0300

    test

--- referral_platform/registrations/management/commands/data_cleaning.py
+++ referral_platform/registrations/management/commands/data_cleaning.py
@@ -6,9 +6,13 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     help = 'Update old registration assessment data'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--filename', type=str, default=None)
+
     def handle(self, *args, **options):
+
         from referral_platform.registrations.models import AssessmentSubmission
-        newmaping = AssessmentSubmission.objects.all()[:90000]
+        newmaping = AssessmentSubmission.objects.filter(assessment__slug='filename')
         for obj in newmaping:
             obj.update_field()
 
