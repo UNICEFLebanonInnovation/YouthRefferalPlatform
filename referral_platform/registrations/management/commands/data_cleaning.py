5fef2f329d5a4fa2d297817b7d4a4ae2d3edda4c
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-10-23 11:14:44 +0300

    tst

--- referral_platform/registrations/management/commands/data_cleaning.py
+++ referral_platform/registrations/management/commands/data_cleaning.py
@@ -2,15 +2,15 @@ __author__ = 'achamseddine'
 
 from django.core.management.base import BaseCommand
 class Command(BaseCommand):
-    help = 'Update old registration assessment data'
+    help = 'Update old registration assessment data, enter id for partner to it on partner level, or par = to All to update all or OE for only empty entries'
 
     def add_arguments(self, parser):
         parser.add_argument('--id', type=int, default=None)
-        parser.add_argument('--all', type=str, default=None)
+        parser.add_argument('--par', type=str, default=None)
 
     def handle(self, *args, **options):
         id = options['id'],
-        all = options['all'],
+        par = options['all'],
 
         # help = 'use all to update all, or new to update only empty new data'
         if id:
@@ -18,12 +18,12 @@ class Command(BaseCommand):
             newmaping = AssessmentSubmission.objects.filter(registration__partner_organization=id)
             for obj in newmaping:
                 obj.update_field()
-        elif all:
+        elif options['all']:
             from referral_platform.registrations.models import AssessmentSubmission
             newmaping = AssessmentSubmission.objects.all()
             for obj in newmaping:
                 obj.update_field()
-        else:
+        elif options['OE']:
             from referral_platform.registrations.models import AssessmentSubmission
             newmaping = AssessmentSubmission.objects.filter(self.new_data == "")
             for obj in newmaping:
