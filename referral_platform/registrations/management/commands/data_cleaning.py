0f44f16eb1c588601baed24e479ce5211d1fc6cc
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-10-23 09:16:45 +0300

    test

--- referral_platform/registrations/management/commands/data_cleaning.py
+++ referral_platform/registrations/management/commands/data_cleaning.py
@@ -1,26 +1,24 @@
 __author__ = 'achamseddine'
 
 from django.core.management.base import BaseCommand
-
-
 class Command(BaseCommand):
     help = 'Update old registration assessment data'
 
     def add_arguments(self, parser):
-        parser.add_argument('--filename', type=str, default=None)
+        parser.add_argument('--id', type=int, default=None)
         parser.add_argument('--updated', type=str, default=None)
 
     def handle(self, *args, **options):
-        filename = options['filename'],
+        id = options['id'],
         updated = options['updated'],
-        # # help = 'use all to update all, or new to update only empty new data'
-        # if filename:
-        #     from referral_platform.registrations.models import AssessmentSubmission
-        #     newmaping = AssessmentSubmission.objects.filter(assessment__slug=filename)
-        #     for obj in newmaping:
-        #         obj.update_field()
-        # else:
-        from referral_platform.registrations.models import AssessmentSubmission
-        newmaping = AssessmentSubmission.objects.filter(registration__partner_organization='Nour')
-        for obj in newmaping:
-            obj.update_field()
+        # help = 'use all to update all, or new to update only empty new data'
+        if id:
+            from referral_platform.registrations.models import AssessmentSubmission
+            newmaping = AssessmentSubmission.objects.filter(registration__partner_organization=id)
+            for obj in newmaping:
+                obj.update_field()
+        else:
+            from referral_platform.registrations.models import AssessmentSubmission
+            newmaping = AssessmentSubmission.objects.all()
+            for obj in newmaping:
+                obj.update_field()
