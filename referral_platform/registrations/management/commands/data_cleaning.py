5da14fd27438f09f8ea60362953532ae5063b0b9
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-10-23 12:59:40 +0300

    tst

--- referral_platform/registrations/management/commands/data_cleaning.py
+++ referral_platform/registrations/management/commands/data_cleaning.py
@@ -5,17 +5,17 @@ class Command(BaseCommand):
     help = 'Update old registration assessment data, enter id for partner to it on partner level, or par = to All to update all or OE for only empty entries'
 
     def add_arguments(self, parser):
-        parser.add_argument('--id', type=int, default=None)
+        parser.add_argument('--partner', type=int, default=None)
         parser.add_argument('--par', type=str, default=None)
 
     def handle(self, *args, **options):
-        id = options['id'],
-        par = options['par'],
+        partner = options['partner']
+        par = options['par']
 
         # help = 'use all to update all, or new to update only empty new data'
-        if id:
+        if partner:
             from referral_platform.registrations.models import AssessmentSubmission
-            newmaping = AssessmentSubmission.objects.filter(registration__partner_organization=id)
+            newmaping = AssessmentSubmission.objects.filter(registration__partner_organization=partner)
             for obj in newmaping:
                 obj.update_field()
         elif par == 'all':
