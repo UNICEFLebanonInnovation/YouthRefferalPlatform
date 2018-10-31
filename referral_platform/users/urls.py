008a09731b30d37c1d3adde63f463ee528c6eb56
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-10-31 12:00:18 +0200

    j

--- referral_platform/users/urls.py
+++ referral_platform/users/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-
+from django.core.urlresolvers import reverse
 from django.conf.urls import url
 
 from . import views
