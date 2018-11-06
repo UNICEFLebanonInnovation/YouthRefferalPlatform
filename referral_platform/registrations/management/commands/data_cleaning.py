f66773705fed524ac00a84670beb991d0976ec87
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-11-06 17:24:18 +0200

    added subsections

--- referral_platform/registrations/management/commands/data_cleaning.py
+++ referral_platform/registrations/management/commands/data_cleaning.py
@@ -1,26 +1,30 @@
 __author__ = 'achamseddine'
 
 from django.core.management.base import BaseCommand
-
-
 class Command(BaseCommand):
-    help = 'Update old registration assessment data'
+    help = 'Update old registration assessment data, enter id for partner to it on partner level, or par = to All to update all or OE for only empty entries'
 
     def add_arguments(self, parser):
-        parser.add_argument('--filename', type=str, default=None)
-        parser.add_argument('--updated', type=str, default=None)
+        parser.add_argument('--partner', type=int, default=None)
+        parser.add_argument('--par', type=str, default=None)
 
     def handle(self, *args, **options):
-        filename = options['filename'],
-        updated = options['updated'],
-        # # help = 'use all to update all, or new to update only empty new data'
-        # if filename:
-        #     from referral_platform.registrations.models import AssessmentSubmission
-        #     newmaping = AssessmentSubmission.objects.filter(assessment__slug=filename)
-        #     for obj in newmaping:
-        #         obj.update_field()
-        # else:
-        from referral_platform.registrations.models import AssessmentSubmission
-        newmaping = AssessmentSubmission.objects.filter(registration__partner_organization=25)
-        for obj in newmaping:
-            obj.update_field()
+        partner = options['partner']
+        par = options['par']
+
+        # help = 'use all to update all, or new to update only empty new data'
+        if partner:
+            from referral_platform.registrations.models import AssessmentSubmission
+            newmaping = AssessmentSubmission.objects.filter(registration__partner_organization=partner)
+            for obj in newmaping:
+                obj.update_field()
+        elif par == 'all':
+            from referral_platform.registrations.models import AssessmentSubmission
+            newmaping = AssessmentSubmission.objects.all()
+            for obj in newmaping:
+                obj.update_field()
+        elif par == 'OE':
+            from referral_platform.registrations.models import AssessmentSubmission
+            newmaping = AssessmentSubmission.objects.filter(new_data__isnull=True)
+            for obj in newmaping:
+                obj.update_field()
