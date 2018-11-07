eb378340fe03560d09a48440e04e48349ae464ba
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-11-07 14:37:49 +0200

    erg

--- referral_platform/registrations/views.py
+++ referral_platform/registrations/views.py
@@ -171,6 +171,7 @@ class AddView(LoginRequiredMixin, FormView):
         beneficiary_flag = self.request.user.is_beneficiary
         if beneficiary_flag:
             reverse('registrations:edit', kwargs={'pk': self.request.GET.get('youth_id')})
+            print (self.request.GET.get('youth_id'))
             return super(AddView, self).form_valid(form)
             # registration_form = Assessment.objects.get(slug="registration")
 
