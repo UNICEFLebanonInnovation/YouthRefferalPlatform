839045e51680bd323c217255d08ba2e3f3a82c00
Rami Sweidane <rsweidan@Ramis-MBP.lan>
2018-11-24 18:45:40 +0200

    nhv

--- referral_platform/registrations/views.py
+++ referral_platform/registrations/views.py
@@ -178,7 +178,7 @@ class AddView(LoginRequiredMixin, FormView):
         else:
             form_class = PartnerCommonForm
 
-        instance = Registration.objects.get(id=self.request.GET.get('owner', 'partner_organization'))
+        instance = Registration.objects.get(id=self.request.GET.get('owner'))
         if self.request.method == "POST":
             return form_class(self.request.POST, instance=instance)
         else:
