8950a98f1e31af18cf4aa75e8960a166387714a6
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-12-14 10:26:15 +0200

    test

--- referral_platform/INIT/model.py
+++ referral_platform/INIT/model.py
@@ -10,13 +10,13 @@ from referral_platform.partners.models import PartnerOrganization
 
 class YLInitiative(models.Model):
 
-    owner = models.ForeignKey(PartnerOrganization, blank=True, null=True)
-    name = models.CharField(max_length=100)
-    overview = models.TextField(blank=True, null=True)
-    start_date = models.DateField(blank=True, null=True)
-    end_date = models.DateField(blank=True, null=True)
-    capacity = models.IntegerField(blank=True, null=True)
-    assessment_form = models.URLField(blank=True, null=True)
+    partner_organization = models.ForeignKey(PartnerOrganization, blank=True, null=True)
+    title = models.CharField(max_length=100)
+    # overview = models.TextField(blank=True, null=True)
+    # start_date = models.DateField(blank=True, null=True)
+    # end_date = models.DateField(blank=True, null=True)
+    # capacity = models.IntegerField(blank=True, null=True)
+    # assessment_form = models.URLField(blank=True, null=True)
     members = models.ForeignKey(YoungPerson)
     location = models.ForeignKey(Location, blank=True, null=True)
 
