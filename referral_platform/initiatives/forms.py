c98af7791b70890fdc7d5ca7e3d7d70e612ceffb
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-12-14 14:06:35 +0200

    tst

--- referral_platform/initiatives/forms.py
+++ referral_platform/initiatives/forms.py
@@ -2,11 +2,17 @@ from __future__ import unicode_literals, absolute_import, division
 
 from django.utils.translation import ugettext as _
 from django import forms
+from django.core.urlresolvers import reverse
+from django.contrib import messages
+
+from dal import autocomplete
+from crispy_forms.helper import FormHelper
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.layout import Layout, Fieldset, Button, Submit, Div, Field, HTML
 
 from model_utils import Choices
 from crispy_forms.helper import FormHelper
 from crispy_forms.bootstrap import FormActions, Accordion, PrependedText, InlineRadios, InlineField, Alert
-from crispy_forms.layout import Layout, Fieldset, Button, Submit, Div, Field, HTML
 from bootstrap3_datetime.widgets import DateTimePicker
 
 from .models import YouthLedInitiative
@@ -38,6 +44,12 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(YouthLedInitiativePlanningForm, self).__init__(*args, **kwargs)
 
+
+
+        instance = kwargs['instance'] if 'instance' in kwargs else ''
+
+        display_registry = ''
+
         self.fields['partner_organization'].empty_label = _('Partner Organisation')
         self.fields['location'].empty_label = _('Select Location for the initiative')
         self.fields['duration'].empty_label = _('Duration of the initiative')
@@ -45,80 +57,87 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
         self.helper = FormHelper()
         self.helper.form_show_labels = False
         self.helper.layout = Layout(
-            Div(
-                Div('partner_organization', css_class='col-md-6', ),
-                Div(PrependedText('title', _('Initiative Title')), css_class='col-md-6', ),
-                css_class='row',
-            ),
-            HTML(_('How many group members planned the initiative?')),
-            'members',
-            'location',
-            Div(
-                Div(PrependedText('start_date', _('Planned start date')), css_class='col-md-6', ),
-                Div('duration', css_class='col-md-6', ),
-                css_class='row',
-            ),
-            Alert(_('The below question is to be answered by the team representative of the initiative'), dismiss=False),
-            Div(
-                Div(
-                    HTML(_('Type of Initiative: (Select All That Apply)')),
-                    Div('initiative_type'),
-                    css_class='col-md-6'),
-                Div(
-                    HTML(_('Can you please select the skills that your team used to plan the initiative?')),
-                    Div('skill_areas'),
-                    css_class='col-md-6'),
-                css_class='row',
-            ),
-            HTML(_('What problem are you solving? How did you choose to do this initiative?')),
-            Field('why_this_initiative', placeholder=_('not more than 5 sentences')),
-
-            HTML(_('What organizations, groups, or other people will you collaborate with on this project?')),
-            'other_groups',
-
-            HTML(_('How many direct beneficiaries will benefit from your planned initiative?')),
-            'number_of_beneficiaries',
-
-            HTML(_('Please select the age Groups of the beneficiaries')),
-            'age_of_beneficiaries',
-
-            HTML(_('Sex of beneficiaries')),
-            'sex_of_beneficiaries',
-
-            HTML(_('How many indirect beneficiaries will benefit from your planned initiative?')),
-            'indirect_beneficiaries',
-
-            HTML(_('Are you planning to mobilize resources for this project?')),
-            'needs_resources',
-
-            HTML(_('If so, from whom?')),
-            'resources_from',
-
-            HTML(_('What kind of support are you planning to receive? (you can select more than one option)')),
-            'resources_type',
-
-            HTML(_('Can you please tell us what your initiative is about, its objective and why is it important?')),
-            Field('description', placeholder=_('not more than 5 sentences')),
-
-            HTML(_('Can you please tell us your planned results?')),
-            Field('planned_results', placeholder=_('not more than 5 sentences')),
-
-            HTML(_('Can you please tell us your anticipated challenges?')),
-            Field('anticpated_challenges', placeholder=_('not more than 5 sentences')),
-
-            HTML(_('How are you planning to mitigate those challenges?')),
-            Field('mitigation_of_challenges', placeholder=_('not more than 5 sentences')),
-
-            HTML(_('How are you planning to measure progress achieved against your results?')),
-            Field('how_to_measure_progress', placeholder=_('not more than 5 sentences')),
-
-            HTML(_('How will you ensure the project is sustainable?')),
-            Field('how_to_ensure_sustainability', placeholder=_('not more than 5 sentences')),
+            # Div(
+            #     Div('partner_organization', css_class='col-md-6', ),
+            #     Div(PrependedText('title', _('Initiative Title')), css_class='col-md-6', ),
+            #     css_class='row',
+            # ),
+            # HTML(_('How many group members planned the initiative?')),
+            # 'members',
+            # 'location',
+            # Div(
+            #     Div(PrependedText('start_date', _('Planned start date')), css_class='col-md-6', ),
+            #     Div('duration', css_class='col-md-6', ),
+            #     css_class='row',
+            # ),
+            # Alert(_('The below question is to be answered by the team representative of the initiative'), dismiss=False),
+            # Div(
+            #     Div(
+            #         HTML(_('Type of Initiative: (Select All That Apply)')),
+            #         Div('initiative_type'),
+            #         css_class='col-md-6'),
+            #     Div(
+            #         HTML(_('Can you please select the skills that your team used to plan the initiative?')),
+            #         Div('skill_areas'),
+            #         css_class='col-md-6'),
+            #     css_class='row',
+            # ),
+            # HTML(_('What problem are you solving? How did you choose to do this initiative?')),
+            # Field('why_this_initiative', placeholder=_('not more than 5 sentences')),
+            #
+            # HTML(_('What organizations, groups, or other people will you collaborate with on this project?')),
+            # 'other_groups',
+            #
+            # HTML(_('How many direct beneficiaries will benefit from your planned initiative?')),
+            # 'number_of_beneficiaries',
+            #
+            # HTML(_('Please select the age Groups of the beneficiaries')),
+            # 'age_of_beneficiaries',
+            #
+            # HTML(_('Sex of beneficiaries')),
+            # 'sex_of_beneficiaries',
+            #
+            # HTML(_('How many indirect beneficiaries will benefit from your planned initiative?')),
+            # 'indirect_beneficiaries',
+            #
+            # HTML(_('Are you planning to mobilize resources for this project?')),
+            # 'needs_resources',
+            #
+            # HTML(_('If so, from whom?')),
+            # 'resources_from',
+            #
+            # HTML(_('What kind of support are you planning to receive? (you can select more than one option)')),
+            # 'resources_type',
+            #
+            # HTML(_('Can you please tell us what your initiative is about, its objective and why is it important?')),
+            # Field('description', placeholder=_('not more than 5 sentences')),
+            #
+            # HTML(_('Can you please tell us your planned results?')),
+            # Field('planned_results', placeholder=_('not more than 5 sentences')),
+            #
+            # HTML(_('Can you please tell us your anticipated challenges?')),
+            # Field('anticpated_challenges', placeholder=_('not more than 5 sentences')),
+            #
+            # HTML(_('How are you planning to mitigate those challenges?')),
+            # Field('mitigation_of_challenges', placeholder=_('not more than 5 sentences')),
+            #
+            # HTML(_('How are you planning to measure progress achieved against your results?')),
+            # Field('how_to_measure_progress', placeholder=_('not more than 5 sentences')),
+            #
+            # HTML(_('How will you ensure the project is sustainable?')),
+            # Field('how_to_ensure_sustainability', placeholder=_('not more than 5 sentences')),
 
             FormActions(
                 Submit('save', _('Save')),
-                Button('cancel', _('Cancel'))
+                HTML('<a class="btn btn-info cancel-button" href="/enrollments/list/" translation="' +
+                     _('Are you sure you want to cancel this registration?') + '">' + _('Back to list') + '</a>'),
             )
         )
+
+    def save(self, instance=None, request=None):
+        instance = super(YouthLedInitiativePlanningForm, self).save()
+        messages.success(request, _('Your data has been sent successfully to the server'))
+
+
 
 
