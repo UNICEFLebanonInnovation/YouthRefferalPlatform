5404d90a651e8df9c84482b25f86c7ca13869381
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-12-15 11:37:32 +0200

    y

--- referral_platform/courses/urls.py
+++ referral_platform/courses/urls.py
@@ -1,13 +1,13 @@
-
-from django.conf.urls import include, url
-from django.views.generic import TemplateView
-
-from .views import CoursesOverview, CourseAssessment, CourseAssessmentSubmission
-
-urlpatterns = [
-
-
-    url(regex=r'^add/$', view=CoursesOverview.as_view(), name='add-initiative'),
-    url(r'^(?P<path>[-\w]+)/(?P<course>[-\w]+)$', CourseAssessment.as_view(), name='assessment'),
-    url(r'^(?P<path>[-\w]+)/(?P<course>[-\w]+)/submit$', CourseAssessmentSubmission.as_view())
-]
+#
+# from django.conf.urls import include, url
+# from django.views.generic import TemplateView
+#
+# from .views import CoursesOverview, CourseAssessment, CourseAssessmentSubmission
+#
+# urlpatterns = [
+#
+#
+#     url(regex=r'^add/$', view=CoursesOverview.as_view(), name='add-initiative'),
+#     url(r'^(?P<path>[-\w]+)/(?P<course>[-\w]+)$', CourseAssessment.as_view(), name='assessment'),
+#     url(r'^(?P<path>[-\w]+)/(?P<course>[-\w]+)/submit$', CourseAssessmentSubmission.as_view())
+# ]
