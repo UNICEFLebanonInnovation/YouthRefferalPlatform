ea34890beb58200419e4682158ee4e68541fbace
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2018-12-16 16:42:55 +0200

    fdf

--- referral_platform/initiatives/forms.py
+++ referral_platform/initiatives/forms.py
@@ -10,11 +10,33 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.bootstrap import FormActions
 from crispy_forms.layout import Layout, Fieldset, Button, Submit, Div, Field, HTML
 
+
+from django import forms
+from django.core.urlresolvers import reverse
+from django.db.models import Q
+from django.contrib import messages
+from django.utils.translation import ugettext as _t
+from django.utils.translation import ugettext_lazy as _
+
+from datetime import datetime
+from collections import OrderedDict
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Fieldset, Submit, Div, HTML, Layout
+
+from referral_platform.locations.models import Location
+from referral_platform.partners.models import Center
+from referral_platform.registrations.models import Assessment, AssessmentSubmission
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+from referral_platform.youth.models import YoungPerson, Nationality, Center
+from referral_platform.registrations.models import Registration
+from django.utils.safestring import mark_safe
 from model_utils import Choices
 from crispy_forms.helper import FormHelper
 from crispy_forms.bootstrap import FormActions, Accordion, PrependedText, InlineRadios, InlineField, Alert
 from bootstrap3_datetime.widgets import DateTimePicker
-
 from .models import YouthLedInitiative, YoungPerson, Location
 
 YES_NO_CHOICE = ((False, _('No')), (True, _('Yes')))
@@ -83,7 +105,7 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
             HTML(_('Initiative Type')),
             'initiative_types',
             HTML(_('Duration')),
-            'duration',
+            'duration',)
             # Div(
             #     Div(PrependedText('start_date', _('Planned start date')), css_class='col-md-6', ),
             #     Div('duration', css_class='col-md-6', ),
@@ -145,10 +167,103 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
             #
             # HTML(_('How will you ensure the project is sustainable?')),
             # Field('how_to_ensure_sustainability', placeholder=_('not more than 5 sentences')),
-
+        # Rendering the assessments
+        # Rendering the assessments
+        #     if instance:
+        #         form_action = reverse('registrations:edit', kwargs={'pk': instance.id})
+        #         all_forms = Assessment.objects.filter(Q(partner__isnull=True) | Q(partner=partner))
+        #         new_forms = OrderedDict()
+        #
+        #     registration_form = Assessment.objects.get(slug="registration")
+        #
+        #     youth_registered = AssessmentSubmission.objects.filter(
+        #         assessment_id=registration_form.id,
+        #         registration_id=instance.id
+        #     ).exists()
+        #
+        #     for specific_form in all_forms:
+        #         formtxt = '{assessment}?registry={registry}'.format(
+        #             assessment=reverse('registrations:assessment', kwargs={'slug': specific_form.slug}),
+        #             registry=instance.id,
+        #         )
+        #     disabled = ""
+        #
+        #     if youth_registered:
+        #         if
+        #     specific_form.slug == "registration":
+        #     disabled = "disabled"
+        #     # check if the pre is already filled
+        #     else:
+        #     order = 1  # int(specific_form.order.split(".")[1])
+        #     if order == 1:
+        #     # If the user filled the form disable it
+        #         form_submitted = AssessmentSubmission.objects.filter(
+        #             assessment_id=specific_form.id, registration_id=instance.id).exists()
+        #     if form_submitted:
+        #         disabled = "disabled"
+        #     else:
+        #     # make sure the user filled the form behind this one in order to enable it
+        #         if
+        #     previous_status == "disabled":
+        #     previous_submitted = AssessmentSubmission.objects.filter(
+        #         assessment_id=specific_form.id, registration_id=instance.id).exists()
+        #     if previous_submitted:
+        #         disabled = "disabled"
+        #     else:
+        #         disabled = "disabled"
+        #     else:
+        #     if specific_form.slug != "registration":
+        #         disabled = "disabled"
+        #
+        #     if specific_form.name not in new_forms:
+        #         new_forms[specific_form.name] = OrderedDict()
+        #     new_forms[specific_form.name][specific_form.order] = {
+        #         'title': specific_form.overview,
+        #         'form': formtxt,
+        #         'overview': specific_form.name,
+        #         'disabled': disabled
+        #     }
+        #     previous_status = disabled
+        #     assessment_fieldset = []
+        #
+        #     for name in new_forms:
+        #         test_html = ""
+        #
+        #     for test_order in new_forms[name]:
+        #         test_html = test_html + '<div class="col-md-3"><a class="btn btn-success ' \
+        #                     + new_forms[name][test_order]['disabled'] + '" href="' + \
+        #                     new_forms[name][test_order][
+        #                         'form'] \
+        #                     + '">' + new_forms[name][test_order][
+        #                         'title'] + '</a></div> '
+        #     assessment_div = Div(
+        #         HTML(test_html),
+        #         css_class='row'
+        #     )
+        #     test_fieldset = Fieldset(
+        #         None,
+        #         Div(
+        #             HTML('<h4 id="alternatives-to-hidden-labels">' + new_forms[name][test_order][
+        #                 'overview'] + '</h4>')
+        #         ),
+        #         assessment_div,
+        #         Div(
+        #             HTML('<div class="p-3"></div>'),
+        #             css_class='row'
+        #         ),
+        #         css_class='bd-callout bd-callout-warning'
+        #     )
+        #     assessment_fieldset.append(test_fieldset)
+        #     for myflds in assessment_fieldset:
+        #         self.helper.layout.append(myflds)
+        #
+        # self.helper.form_action = form_action
+        self.helper.layout.append(
             FormActions(
-                Submit('save_add_another', _('Save and Add Another')),
-                Button('cancel', _('Cancel'))
+                HTML('<a class="btn btn-info col-md-2" href="/initiatives/list/">' + _t('Cancel') + '</a>'),
+                Submit('save_and_continue', _('Save and continue'), css_class='col-md-2'),
+                css_class='btn-actions'
+
             )
         )
 
