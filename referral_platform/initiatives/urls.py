d18c9fbdea8e2ec0eb9f2ab1ff062f56a07c2d5a
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-01-02 12:41:12 +0200

    dt

--- referral_platform/initiatives/urls.py
+++ referral_platform/initiatives/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
         name='edit'
     ),
     url(
-        regex=r'^assessment/(?P<slug>[\w.@+-]+)/$',
+        regex=r'^assessment/list/(?P<slug>[\w.@+-]+)/$',
         view=views.YouthAssessment.as_view(),
         name='assessment'
     ),
