c4fe488c9b981f73a6781b713d13ff216fb2c5c8
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-01-09 16:41:35 +0200

    test

--- referral_platform/initiatives/views.py
+++ referral_platform/initiatives/views.py
@@ -20,7 +20,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import SingleObjectMixin
 from django.views.generic import RedirectView
 from django.shortcuts import render
-
+from referral_platform.backends.djqscsv import render_to_csv_response
+from rest_framework import status
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
@@ -194,3 +195,182 @@ class YouthAssessment(SingleObjectMixin, RedirectView):
 #         submission.save()
 #
 #         return HttpResponse()
+
+class ExportInitiativeAssessmentsView(LoginRequiredMixin, ListView):
+
+    model = AssessmentSubmission
+    queryset = AssessmentSubmission.objects.filter(assessment__slug__in=['init_exec', 'init_registration'])
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            queryset = self.queryset.filter(
+                initiative__governorate__parent__name_en=self.request.user.country.name_en)
+        else:
+            queryset = self.queryset.filter(initiative__partner_organization=self.request.user.partner)
+
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+
+        headers = {
+
+            'respid_initiativeID_title': 'Initiative Title',
+            'initiative_loc': 'Initiative Location',
+            'gender_implem_initiatives': 'Gender of members who were engaged in the initiative implementation',
+            'No_of_team_members_executed': 'Number of people engaged in the initiative implementation',
+            # 'integer_0259d46e': 'How many people benefited/ reached by implementing the initiative?',
+            'start_date_implementing_initia': 'Planned start date of the initiative',
+            'type_of_initiative': 'Type of Initiative',
+            'other_type_of_initiative': 'If other, please specify',
+            'duration_of_initiative': 'Duration of the initiative',
+            # 'select_multiple_e160966a': 'The Age groups of the beneficiaries reached?',
+            # 'select_one_a3c4ea99': 'Sex of beneficiaries',
+            'leadership': 'The group members expects to play leading roles for the implementation of the initiative ',
+            'challenges_faced': 'Types of challenges while implementing the initiative',
+            'other_challenges': 'Others, please specify',
+            'number_of_direct_beneficiaries': 'How many people are estimated to benefit/will be reached by implementing the initiative?',
+            'age_group_range': 'The estimated Age groups of the beneficiaries?',
+            'gender_of_beneficiaries': 'Gender of beneficiaries',
+            'mentor_assigned': 'Did your group have a mentor/facilitator/teacher to support you with planning of the initiative?',
+            'initiative_as_expected': 'The team expects to implement the initiative as expected',
+            'team_involovement': 'Team members expect to participate effectively in the implementation of the initiative',
+            'communication': 'The group aims to communicate with each other for the implementation of the initiative',
+            #'analytical_skills': 'The group expects to collect and analyse data for the implementation of the initiative ',
+            'analytical_skills': 'The group expects to collect and analyse data for the implementation of the initiative',
+            'sense_of_belonging': 'The group expects to have a sense of belonging while implementing of the initiatives',
+            'problem_solving': 'The group is confident in coming up with solutions if challenges are faced',
+            'assertiveness': 'The group feels certain that the initiative will address the problem(s) faced by our communities',
+            'mentorship_helpful': 'The group expects to find the mentorship in the planning phase very helpful',
+            'problem_addressed': 'Can you tell us more about the problem you/your community is facing?',
+            'planned_results': 'Can you please tell us your planned results/what will the initiative achieve? ',
+            'planning_to_mobilize_resources': 'Are you planning to mobilize resources for this project?',
+            'mobilized_resources_through': 'If so, from whom?',
+            'did_you_mobilize_resources': 'Were resources mobilized for this project?',
+            '_geolocation': 'Location',
+            'if_so_who': 'If yes, from whom?',
+
+            'type_of_support_required': 'What kind of support are you planning to receive?',
+            'type_of_support_received': 'What kind of support did you receive?',
+            'support_received_helpful': 'The support we received was helpful and consistent with what the group was expecting',
+            'support_not_helpful_why': 'If you answer is disagree or strongly disagree, can you tell us why?',
+
+            'start': 'Start Date',
+            'end': 'End ',
+            '_submission_time': 'submission time',
+            '_userform_id': 'Registered by',
+        }
+
+        qs = self.get_queryset().extra(select={
+
+            'respid_initiativeID_title': "new_data->>'respid_initiativeID_title'",
+            'initiative_loc': "new_data->>'initiative_loc'",
+            'gender_implem_initiatives': "new_data->>'gender_implem_initiatives'",
+            'No_of_team_members_executed': "new_data->>'No_of_team_members_executed'",
+            # 'integer_0259d46e': "new_data->>'integer_0259d46e'",
+            'start_date_implementing_initia': "new_data->>'start_date_implementing_initia'",
+            'type_of_initiative': "new_data->>'type_of_initiative'",
+            'other_type_of_initiative': "new_data->>'other_type_of_initiative'",
+            'duration_of_initiative': "new_data->>'duration_of_initiative'",
+            # 'select_multiple_e160966a': "new_data->>'select_multiple_e160966a'",
+            # 'select_one_a3c4ea99': "new_data->>'select_one_a3c4ea99'",
+            'leadership': "new_data->>'leadership'",
+            'challenges_faced': "new_data->>'challenges_faced'",
+            'other_challenges': "new_data->>'other_challenges'",
+            'number_of_direct_beneficiaries': "new_data->>'number_of_direct_beneficiaries'",
+            'age_group_range': "new_data->>'age_group_range'",
+            'gender_of_beneficiaries': "new_data->>'gender_of_beneficiaries'",
+            'mentor_assigned': "new_data->>'mentor_assigned'",
+            'initiative_as_expected': "new_data->>'initiative_as_expected'",
+            'team_involovement': "new_data->>'team_involovement'",
+            'communication': "new_data->>'communication'",
+            'problem_solving': "new_data->>'problem_solving'",
+            'analytical_skills': "new_data->>'analytical_skills'",
+            'sense_of_belonging': "new_data->>'sense_of_belonging'",
+            'assertiveness': "new_data->>'assertiveness'",
+            'mentorship_helpful': "new_data->>'mentorship_helpful'",
+            'problem_addressed': "new_data->>'problem_addressed'",
+            'planned_results': "new_data->>'planned_results'",
+            'planning_to_mobilize_resources': "new_data->>'planning_to_mobilize_resources'",
+            'mobilized_resources_through': "new_data->>'mobilized_resources_through'",
+            'did_you_mobilize_resources': "new_data->>'did_you_mobilize_resources'",
+            '_geolocation': "new_data->>'_geolocation'",
+            'if_so_who': "new_data->>'if_so_who'",
+
+            'type_of_support_required': "new_data->>'type_of_support_required'",
+            'type_of_support_received': "new_data->>'type_of_support_received'",
+            'support_received_helpful': "new_data->>'support_received_helpful'",
+            'support_not_helpful_why': "new_data->>'support_not_helpful_why'",
+
+            'start': "new_data->>'start'",
+            'end': "new_data->>'end'",
+            '_submission_time': "new_data->>'_submission_time'",
+            '_userform_id': "new_data->>'_userform_id'",
+
+        }).values(
+            'registration__youth__first_name',
+            'registration__youth__father_name',
+            'registration__youth__last_name',
+            'registration__partner_organization__name',
+            'registration__youth__bayanati_ID',
+            'registration__youth__birthday_day',
+            'registration__youth__birthday_month',
+            'registration__youth__birthday_year',
+            'registration__youth__nationality__code',
+            'registration__youth__marital_status',
+            'registration__youth__sex',
+            'registration__youth__number',
+            'registration__governorate__parent__name_en',
+            'registration__governorate__name_en',
+            'registration__center__name',
+            'registration__location',
+            'assessment__overview',
+
+            'respid_initiativeID_title',
+            'initiative_loc',
+            'gender_implem_initiatives',
+            'No_of_team_members_executed',
+            # 'integer_0259d46e',
+            'start_date_implementing_initia',
+            'type_of_initiative',
+            'other_type_of_initiative',
+            'duration_of_initiative',
+            # 'select_multiple_e160966a',
+            # 'select_one_a3c4ea99',
+            'leadership',
+            'challenges_faced',
+            'other_challenges',
+            'number_of_direct_beneficiaries',
+            'age_group_range',
+            'gender_of_beneficiaries',
+            'mentor_assigned',
+            'initiative_as_expected',
+            'team_involovement',
+            'communication',
+            #'analytical_skills',
+            'analytical_skills',
+            'sense_of_belonging',
+            'problem_solving',
+            'assertiveness',
+            'mentorship_helpful',
+            'problem_addressed',
+            'planned_results',
+            'planning_to_mobilize_resources',
+            'mobilized_resources_through',
+            'did_you_mobilize_resources',
+            '_geolocation',
+            'if_so_who',
+
+            'type_of_support_required',
+            'type_of_support_received',
+            'support_received_helpful',
+            'support_not_helpful_why',
+
+            'start',
+            'end',
+            '_submission_time',
+            '_userform_id',
+        )
+
+        filename = 'Initiative-Export'
+
+        return render_to_csv_response(qs, filename,  field_header_map=headers)
