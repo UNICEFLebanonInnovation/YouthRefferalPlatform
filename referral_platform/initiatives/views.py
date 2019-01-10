de8d9f9c3a5d80bead81f20c94b44ca75ce5eb13
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-01-10 16:31:57 +0200

    exp4

--- referral_platform/initiatives/views.py
+++ referral_platform/initiatives/views.py
@@ -215,8 +215,8 @@ class ExportInitiativeAssessmentsView(LoginRequiredMixin, ListView):
 
             'initiative__title': 'Initiative Title',
             'initiative__location': 'Initiative Location',
-            'initiative__member____first_name': 'First Name',
-            'initiative__member____last_name': 'Last Name',
+            'initiative__member__first_name': 'First Name',
+            'initiative__member__last_name': 'Last Name',
             'initiative__type': 'Type of Initiative',
             'initiative__duration': 'Duration of the initiative',
             'assertiveness': 'The group feels certain that the initiative will address the problem(s) faced by our communities',
@@ -237,8 +237,8 @@ class ExportInitiativeAssessmentsView(LoginRequiredMixin, ListView):
 
         }).values(
             'initiative__title',
-            'initiative__member____first_name',
-            'initiative__member____last_name',
+            'initiative__member__first_name',
+            'initiative__member__last_name',
             'initiative__location',
             'initiative__type',
             'initiative__duration',
