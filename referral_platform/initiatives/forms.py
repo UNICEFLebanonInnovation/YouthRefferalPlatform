59430767caa40a47c496806bd0728f76047b67a0
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-01-22 15:21:27 +0200

    asd

--- referral_platform/initiatives/forms.py
+++ referral_platform/initiatives/forms.py
@@ -3,42 +3,18 @@ from __future__ import unicode_literals, absolute_import, division
 from django.utils.translation import ugettext as _
 from django import forms
 from django.core.urlresolvers import reverse
-from django.contrib import messages
-from collections import OrderedDict
-from dal import autocomplete
-from crispy_forms.helper import FormHelper
-from crispy_forms.bootstrap import FormActions
-from crispy_forms.layout import Layout, Fieldset, Button, Submit, Div, Field, HTML
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.admin import site, ModelAdmin
-from django import forms
-from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.contrib import messages
 from django.utils.translation import ugettext as _t
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.admin.widgets import FilteredSelectMultiple
-from datetime import datetime
 from collections import OrderedDict
-from crispy_forms.bootstrap import FormActions
-from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Fieldset, Submit, Div, HTML, Layout
-
-from referral_platform.locations.models import Location
-from referral_platform.partners.models import Center
 from referral_platform.registrations.models import Assessment, Registration
-from fuzzywuzzy import fuzz
-from fuzzywuzzy import process
-
-from referral_platform.youth.models import YoungPerson, Nationality, Center
-from referral_platform.registrations.models import Registration
-from django.utils.safestring import mark_safe
-from model_utils import Choices
 from crispy_forms.helper import FormHelper
 from crispy_forms.bootstrap import FormActions, Accordion, PrependedText, InlineRadios, InlineField, Alert
-from bootstrap3_datetime.widgets import DateTimePicker
-from .models import YouthLedInitiative, YoungPerson, Location, AssessmentSubmission
+from .models import YouthLedInitiative, YoungPerson, Location
+from referral_platform.initiatives.models import AssessmentSubmission
 
 YES_NO_CHOICE = ((False, _('No')), (True, _('Yes')))
 
@@ -126,128 +102,8 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
             HTML(_('Duration')),
             'duration',
 
-            # HTML(_('Knowledge Areas')),
-            # 'knowledge_areas',
-
-            # HTML(_('Why this initiative')),
-            # 'why_this_initiative',
-
-            # HTML(_('Other Groups')),
-            # 'other_groups',
-
-            # HTML(_('Number of beneficiaries')),
-            # 'number_of_beneficiaries',
-            #
-            # HTML(_('Age of beneficiaries')),
-            # 'age_of_beneficiaries',
-
-            # HTML(_('Sex of beneficiaries')),
-            # 'sex_of_beneficiaries',
-            #
-            # HTML(_('Indirect beneficiaries')),
-            # 'indirect_beneficiaries',
-            #
-            # HTML(_('Needing Resources')),
-            # 'needs_resources',
-            #
-            # HTML(_('Resources From')),
-            # 'resources_from',
-            #
-            # HTML(_('Resources Type')),
-            # 'resources_type',
-            #
-            # HTML(_('Description')),
-            # 'description',
-
         )
 
-        # # Rendering the assessments
-        # if instance:
-        #     form_action = reverse('initiatives:edit', kwargs={'pk': instance.id})
-        #     all_forms = Assessment.objects.filter(Q(slug="init_registration") | Q(slug="init_exec"))
-        #     new_forms = OrderedDict()
-        #
-        #     # registration_form = Assessment.objects.get(slug="registration")
-        #
-        #     youth_registered = AssessmentSubmission.objects.filter(
-        #         initiative_id=instance.id
-        #     ).exists()
-        #
-        #     for specific_form in all_forms:
-        #         formtxt = '{assessment}?registry={registry}'.format(
-        #             assessment=reverse('initiatives:assessment', kwargs={'slug': specific_form.slug}),
-        #             registry=instance.id,
-        #         )
-        #         disabled = ""
-        #
-        #         if youth_registered:
-        #             if specific_form.slug == "init_registration":
-        #                 disabled = "disabled"
-        #             # check if the pre is already filled
-        #             else:
-        #                 order = 1  # int(specific_form.order.split(".")[1])
-        #                 if order == 1:
-        #                     # If the user filled the form disable it
-        #                     form_submitted = AssessmentSubmission.objects.filter(
-        #                         assessment_id=specific_form.id, initiative_id=instance.id).exists()
-        #                     if form_submitted:
-        #                         disabled = "disabled"
-        #                 else:
-        #                     # make sure the user filled the form behind this one in order to enable it
-        #                     if previous_status == "disabled":
-        #                         previous_submitted = AssessmentSubmission.objects.filter(
-        #                             assessment_id=specific_form.id, initiative_id=instance.id).exists()
-        #                         if previous_submitted:
-        #                             disabled = "disabled"
-        #                     else:
-        #                         disabled = "disabled"
-        #         else:
-        #             if specific_form.slug != "init_registration":
-        #                 disabled = "disabled"
-        #
-        #         if specific_form.name not in new_forms:
-        #             new_forms[specific_form.name] = OrderedDict()
-        #         new_forms[specific_form.name][specific_form.order] = {
-        #             'title': specific_form.overview,
-        #             'form': formtxt,
-        #             'overview': specific_form.name,
-        #             'disabled': disabled
-        #         }
-        #         previous_status = disabled
-        #     assessment_fieldset = []
-        #
-        #     for name in new_forms:
-        #         test_html = ""
-        #
-        #         for test_order in new_forms[name]:
-        #             test_html = test_html + '<div class="col-md-3"><a class="btn btn-success ' \
-        #                         + new_forms[name][test_order]['disabled'] + '" href="' + new_forms[name][test_order][
-        #                             'form'] \
-        #                         + '">' + new_forms[name][test_order][
-        #                             'title'] + '</a></div> '
-        #         assessment_div = Div(
-        #             HTML(test_html),
-        #             css_class='row'
-        #         )
-        #         test_fieldset = Fieldset(
-        #             None,
-        #             Div(
-        #                 HTML('<h4 id="alternatives-to-hidden-labels">' + new_forms[name][test_order][
-        #                     'overview'] + '</h4>')
-        #             ),
-        #             assessment_div,
-        #             Div(
-        #                 HTML('<div class="p-3"></div>'),
-        #                 css_class='row'
-        #             ),
-        #             css_class='bd-callout bd-callout-warning'
-        #         )
-        #         assessment_fieldset.append(test_fieldset)
-        #
-        #     for myflds in assessment_fieldset:
-        #         self.helper.layout.append(myflds)
-        #
-        # # self.helper.form_action = form_action
         # Rendering the assessments
         if instance:
             form_action = reverse('initiatives:edit', kwargs={'pk': instance.id})
