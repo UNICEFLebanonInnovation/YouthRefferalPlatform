3ab747d07d69af3e158dd003271d17bd28290cec
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-01-27 10:07:08 +0200

    changed according to 22/01 report

--- config/settings/common.py
+++ config/settings/common.py
@@ -99,7 +99,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
+#     'DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
+#
+# SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
+
 
 # SECURITY CONFIGURATION
 X_FRAME_OPTIONS = 'DENY'
