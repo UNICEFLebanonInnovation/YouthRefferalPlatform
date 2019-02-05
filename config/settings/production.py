372174f947ade60c6517718b9960ac61ece353f0
achamseddine <achamseddine@unicef.org>
2019-02-05 15:00:33 +0200

    try something

--- config/settings/production.py
+++ config/settings/production.py
@@ -129,7 +129,7 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 DATABASES['default'] = 'postgres://dbbsanytmxkyzd:b10e9d200d7acb25a26ff7d84d0aa338944cd47e0503a29ad9e63c0e1eab8df5@ec2-50-16-196-238.compute-1.amazonaws.com:5432/d3pr7hqep9t1bk',
 
 if env.bool('DATABASE_SSL_ENABLED', default=False):
-    DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
+    DATABASES['default']['OPTIONS'] = ({'sslmode': 'require'}, )
 
 # CACHING
 # ------------------------------------------------------------------------------
