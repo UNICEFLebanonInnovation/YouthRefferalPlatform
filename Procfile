9dfd6b2a9ae8e35efcc54cbc448d603d6c9a7f40
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-03-04 22:02:06 +0200

    remove all suspicious files

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn config.wsgi:application
-worker: celery worker --app=referral_platform.taskapp --loglevel=info
--- gunicorn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-python /code/manage.py collectstatic --noinput
-/usr/local/bin/gunicorn config.wsgi -w 4 -b 0.0.0.0:8080 --chdir=/code
-
-#!/bin/sh
-#python manage.py migrate
-#python manage.py runserver_plus 0.0.0.0:8000
