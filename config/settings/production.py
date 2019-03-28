a75cbfd9632e44bf3dd87d3d58e31bbbdfd162e5
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-03-28 10:35:04 +0200

    added local to allowed hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -65,7 +65,7 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['leb-ems.azurewebsites.net'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS','http://127.0.0.1', default=['leb-ems.azurewebsites.net'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
