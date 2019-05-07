c9607d499b930ce49c449747e094e42c5dff97f5
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-05-07 15:58:39 +0300

    push

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import unicode_literals
-from django.views.i18n import JavaScriptCatalog
 
 from django.conf import settings
 from django.conf.urls import include, url
@@ -27,7 +25,7 @@ api.register(r'backend-exporter', ExporterViewSet, base_name='backend-exporter')
 schema_view = get_swagger_view(title='EMS API')
 
 urlpatterns = [
-    url(r'^referral_platform/admin/jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+
     # url(r'^', include('referral_platform.youth.urls', namespace='youth')),
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^home/$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
@@ -64,15 +62,9 @@ urlpatterns = [
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
-    import debug_toolbar
     urlpatterns += [
         url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
         url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
         url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
         url(r'^500/$', default_views.server_error),
     ]
-    if 'debug_toolbar' in settings.INSTALLED_APPS:
-        import debug_toolbar
-        urlpatterns = [
-            url(r'^__debug__/', include(debug_toolbar.urls)),
-        ] + urlpatterns
