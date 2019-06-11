56be791319d25569c6b8bc8a53e531ae19db957c
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-06-11 14:36:19 +0300

    rectify

--- referral_platform/initiatives/models.py
+++ referral_platform/initiatives/models.py
@@ -1,21 +1,15 @@
 from __future__ import unicode_literals, absolute_import, division
 
 from django.db import models
-from django.contrib.postgres.fields import ArrayField, JSONField
 from django.utils.translation import ugettext as _
 from model_utils import Choices
 from django.core.urlresolvers import reverse
 from referral_platform.partners.models import PartnerOrganization
 from referral_platform.locations.models import Location
-<<<<<<< HEAD
-from referral_platform.registrations.models import Registration, NewMapping, JSONField, Assessment
-from .utils import generate_hash
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-=======
+
+from referral_platform.registrations.models import JSONField
 from referral_platform.registrations.models import Registration, NewMapping, Assessment
-from multiselectfield import MultiSelectField
->>>>>>> 8d1cf1a212537dde009b2b630aaacfc8e49b2c40
+
 
 
 class YouthLedInitiative(models.Model):
@@ -72,8 +66,6 @@ class YouthLedInitiative(models.Model):
             ('6_plus', _('More than 6 weeks')),
         )
     )
-
-<<<<<<< HEAD
     type = models.CharField(
         max_length=254,
         blank=True,
@@ -82,157 +74,12 @@ class YouthLedInitiative(models.Model):
         choices=INITIATIVE_TYPES,
 
     )
-=======
-    type = MultiSelectField(choices=INITIATIVE_TYPES)
->>>>>>> 8d1cf1a212537dde009b2b630aaacfc8e49b2c40
+
 
     @property
     def get_participants(self):
         return "\n".join([str(p.id) for p in self.Participants.all()])
 
-<<<<<<< HEAD
-    # type = MultiSelectField(choices=INITIATIVE_TYPES)
-
-
-    # knowledge_areas = models.CharField(
-    #         choices=SKILL_AREAS,
-    #         max_length=254,
-    #         blank=True,
-    #         null=True,
-    #     ),
-    #
-    # why_this_initiative = models.TextField(blank=True, null=True)
-
-    # other_groups = models.CharField(
-    #     max_length=254,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('ngos', _('Other NGOs')),
-    #         ('schools', _('Schools')),
-    #         ('municipality', _('Municipality')),
-    #         ('other', _('Other')),
-    #     ),
-    # )
-    #
-    # number_of_beneficiaries = models.CharField(
-    #     max_length=254,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('1-50', _('1-50')),
-    #         ('51-100 ', _('51-100')),
-    #         ('501-1000', _('501-1000')),
-    #         ('1000-plus', _('greater than 1000')),
-    #     )
-    # )
-    # age_of_beneficiaries = models.CharField(
-    #     max_length=254,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('1-6', _('1-6 years')),
-    #         ('7-13', _('7-13 years')),
-    #         ('14-24', _('14-24 years')),
-    #         ('25-50', _('25-50 years')),
-    #         ('50-plus', _('50 years and above')),
-    #     )
-    # )
-    # sex_of_beneficiaries = models.CharField(
-    #     max_length=254,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('both', _('Both male and females')),
-    #         ('male', _('Only males')),
-    #         ('female', _('only female')),
-    #     )
-    # )
-    # indirect_beneficiaries = models.CharField(
-    #     max_length=254,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('1-50', _('1-50')),
-    #         ('51-100 ', _('51-100')),
-    #         ('501-1000', _('501-1000')),
-    #         ('1000-plus', _('greater than 1000')),
-    #     )
-    # )
-    # needs_resources = models.BooleanField(default=False)
-    # resources_from = models.CharField(
-    #     max_length=254,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('unicef', _('UNICEF')),
-    #         ('local ', _('Local Business')),
-    #         ('organization', _('Organisation')),
-    #     )
-    # )
-    #
-    # resources_type = models.CharField(
-    #     max_length=254,
-    #     blank=True, null=True,
-    #     choices=RESOURCE_TYPES
-    # )
-    #
-    # description = models.TextField(blank=True, null=True)
-    # planned_results = models.TextField(blank=True, null=True)
-    # anticpated_challenges = models.TextField(blank=True, null=True)
-    # mitigation_of_challenges = models.TextField(blank=True, null=True)
-    # how_to_measure_progress = models.TextField(blank=True, null=True)
-    # how_to_ensure_sustainability = models.TextField(blank=True, null=True)
-    #
-    # team_participation_rating = models.CharField(
-    #     max_length=100,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('excellent', _('Excellent - All of our team members were committed to the initiative')),
-    #         ('good', _('Good - Most of our team was committed to the initiative')),
-    #         ('low', _('Low - Some team members stopped participating')),
-    #     )
-    # )
-    #
-    # initiative_activities = ArrayField(
-    #     models.CharField(
-    #         choices=INITIATIVE_TYPES,
-    #         max_length=254,
-    #         blank=True,
-    #         null=True,
-    #     ),
-    #     blank=True,
-    #     null=True,
-    # )
-
-    # number_of_beneficiaries_reached = models.CharField(
-    #     max_length=100,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('exceeded', _('Exceeded the total number of direct beneficiaries that we planned to reach (greater than 100%)')),
-    #         ('reached', _('Reached all of the direct beneficiaries planned (100%)')),
-    #         ('half', _('Reached more than half of the direct beneficiaries planned (50% or more)')),
-    #         ('less', _('Reached less than half of the direct beneficiaries planned (Less then 49%)" (50% or more)')),
-    #     )
-    # )
-    # mentor = models.NullBooleanField()
-    # mentor_was_helpful = models.CharField(
-    #     max_length=254,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('yes', _('Yes')),
-    #         ('no', _('No')),
-    #         ('somewhat', _('Somewhat')),
-    #     )
-    # )
-    # support_helpful = models.CharField(
-    #     max_length=254,
-    #     blank=True, null=True,
-    #     choices=Choices(
-    #         ('yes', _('Yes')),
-    #         ('no', _('No')),
-    #         ('somewhat', _('Somewhat')),
-    #     )
-    # )
-    # challenges_face = models.TextField(blank=True, null=True)
-    # lessons_learnt = models.TextField(blank=True, null=True)
-
-=======
->>>>>>> 8d1cf1a212537dde009b2b630aaacfc8e49b2c40
     def get_assessment(self, slug):
         assessment = self.assessmentsubmission_set.filter(assessment__slug=slug).first()
         if assessment:
