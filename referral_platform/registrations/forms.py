41c2e9745d8999cfdbe492d6c4c439925947652a
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-11-06 14:30:49 +0200

    test

--- referral_platform/registrations/forms.py
+++ referral_platform/registrations/forms.py
@@ -296,7 +296,7 @@ class CommonForm(forms.ModelForm):
                         else:
                             # make sure the user filled the form behind this one in order to enable it
                             for specific_form in all_form:
-                                if specific_form.previous_status == "disabled":
+                                if previous_status == "disabled":
                                     previous_submitted = AssessmentSubmission.objects.filter(
                                         assessment_id=specific_form.id, registration_id=instance.id).exists()
                                     if previous_submitted:
