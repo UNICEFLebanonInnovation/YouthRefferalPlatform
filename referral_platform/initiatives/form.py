e8e1709c37127e897828e6649013b2bdd12adbe4
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-13 13:37:49 +0200

    test

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -72,7 +72,7 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
 
 
         self.fields['center'].queryset = Center.objects.filter(partner_organization=partner_organization)
-        self.fields['Participants'].queryset = Registration.objects.filter(partner_organization=partner_organization)
+        # self.fields['Participants'].queryset = Registration.objects.filter(partner_organization=partner_organization)
         self.fields['partner_organization'].widget.attrs['readonly'] = True
         my_fields = OrderedDict()
 
