81d91dbc1e0742f4c882ff211b0275e86f453995
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-15 12:37:48 +0200

    test

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -72,16 +72,17 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
         partner_locations = initials['partner_locations'] if 'partner_locations' in initials else []
         partner_organization = initials['partner_organization'] if 'partner_organization' in initials else 0
         self.fields['governorate'].queryset = Location.objects.filter(parent__in=partner_locations)
-        self.fields['center'].queryset = Center.objects.filter(partner_organization=partner_organization)
+
 
         if initials['center_flag']:
-            self.fields['Participants'].queryset = Registration.objects.filter(
-                center=int(instance.center))
-            self.fields['center'].queryset = initials['center']
+            # self.fields['Participants'].queryset = Registration.objects.filter(
+            #     center=instance.center)
+            self.fields['center'] = instance.center
         else:
             self.fields['Participants'].queryset = Registration.objects.filter(
                 partner_organization=partner_organization)
             self.fields['center'].queryset = Center.objects.filter(partner_organization=partner_organization)
+            self.fields['center'] = Center.objects.filter(partner_organization=partner_organization)
         self.fields['partner_organization'].widget.attrs['readonly'] = True
 
         my_fields = OrderedDict()
