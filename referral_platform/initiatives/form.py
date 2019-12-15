84b294585d58f6a698d90569e88f86c1e2282ff6
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-15 23:00:28 +0200

    test with center id

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -76,7 +76,8 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
             # self.fields['center'].queryset = Center.objects.filter(partner_organization=partner_organization)
             self.fields['Participants'].queryset = Registration.objects.filter(center=initials['center'])
             self.fields['partner_organization'].widget.attrs['readonly'] = True
-            
+            self.fields['center'].widget.attrs['readonly'] = True
+
         else:
             partner_locations = initials['partner_locations'] if 'partner_locations' in initials else []
             partner_organization = initials['partner_organization'] if 'partner_organization' in initials else 0
