f973be6ef337c27586f7dcadb7436809128a02bc
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-15 22:31:19 +0200

    remove center

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -67,7 +67,7 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
         partner_locations = initials['partner_locations'] if 'partner_locations' in initials else []
         partner_organization = initials['partner_organization'] if 'partner_organization' in initials else 0
         self.fields['governorate'].queryset = Location.objects.filter(parent__in=partner_locations)
-        self.fields['center'] = initials['center']
+        # self.fields['center'] = initials['center']
         # self.fields['center'].queryset = Center.objects.filter(partner_organization=partner_organization)
         self.fields['Participants'].queryset = Registration.objects.filter(partner_organization=partner_organization)
         self.fields['partner_organization'].widget.attrs['readonly'] = True
