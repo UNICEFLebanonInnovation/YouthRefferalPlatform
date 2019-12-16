13a8a5ab4130d20471de0ac606990506a439d71d
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-16 15:02:26 +0200

    test

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -57,7 +57,7 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
 
         self.request = kwargs.pop('request', None)
         instance = kwargs.get('instance', '')
-        owner = instance.owner
+        owner = instance.user
         if instance:
             initials = {}
             initials['partner_locations'] = instance.partner_organization.locations.all()
