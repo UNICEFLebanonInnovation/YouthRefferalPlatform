0eb1a9f98414837498b5b76e934437685094d88a
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-17 12:56:24 +0200

    test

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -60,7 +60,7 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
         instance = kwargs.get('instance', '')
         owner = kwargs.get('user_id', '')
         print("this is kwargs")
-        print(kwargs)
+        print(instance)
         print("this is kwargs")
         print (owner)
         print("this is types")
