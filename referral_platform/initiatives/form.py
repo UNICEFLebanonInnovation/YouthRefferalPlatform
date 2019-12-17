875f9783cde658157dab91561be901028b8e0bf8
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-17 09:10:20 +0200

    owner

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -63,12 +63,12 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
             initials['partner_locations'] = instance.partner_organization.locations.all()
             initials['partner_organization'] = instance.partner_organization
             initials['center'] = instance.center
-            user = instance.user
+            owner = instance.user
 
         else:
             initials = kwargs.get('initial', '')
 
-        if user.is_center:
+        if owner.is_center:
             partner_locations = initials['partner_locations'] if 'partner_locations' in initials else []
             partner_organization = initials['partner_organization'] if 'partner_organization' in initials else 0
             self.fields['governorate'].queryset = Location.objects.filter(parent__in=partner_locations)
