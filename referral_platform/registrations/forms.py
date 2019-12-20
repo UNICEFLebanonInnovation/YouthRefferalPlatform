dbac7d0654d26232429c91e2c9d65be97fa48265
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-20 16:25:11 +0200

    ewr

--- referral_platform/registrations/forms.py
+++ referral_platform/registrations/forms.py
@@ -266,7 +266,7 @@ class CommonForm(forms.ModelForm):
         # Rendering the assessments
         if instance:
             form_action = reverse('registrations:edit', kwargs={'pk': instance.id})
-            all_forms = Assessment.objects.filter(location=int(country))
+            all_forms = Assessment.objects.filter(location_id=int(country))
             # all_forms = all_forms.filter(location=country)
             # country_form = all_forms.filter(location_id=country)
             new_forms = OrderedDict()
