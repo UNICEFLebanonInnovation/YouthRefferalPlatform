c654072b2dbfe71449f3c5b14b831001fb873de9
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-26 17:43:44 +0200

    test

--- referral_platform/locations/urls.py
+++ referral_platform/locations/urls.py
@@ -1,10 +1,8 @@
-from __future__ import absolute_import, unicode_literals
-
 from django.conf.urls import url
-
 from . import views
-
 from .autocompletes import LocationAutocomplete
+
+
 
 urlpatterns = [
     url(r'^location-autocomplete/$',
