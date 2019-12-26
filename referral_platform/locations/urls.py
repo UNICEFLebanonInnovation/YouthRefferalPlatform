682ccaf26e99a4bd83ba1f3f90aeb5d863f22a32
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-26 20:03:11 +0200

    revert

--- referral_platform/locations/urls.py
+++ referral_platform/locations/urls.py
@@ -1,18 +1,14 @@
+from __future__ import absolute_import, unicode_literals
+
 from django.conf.urls import url
+
 from . import views
+
 from .autocompletes import LocationAutocomplete
-
-
 
 urlpatterns = [
     url(r'^location-autocomplete/$',
         LocationAutocomplete.as_view(),
         name='location-autocomplete',
         ),
-
-    url(
-        regex=r'^exec-sequence/$',
-        view=views.ExecSequenceView.as_view(),
-        name='exec_sequence'
-    ),
 ]
