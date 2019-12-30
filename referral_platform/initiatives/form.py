c0d74efe2bda810fd66e6b4f371bc2a34153863e
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2019-12-30 21:01:34 +0200

    test

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -127,7 +127,7 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
         if instance:
             form_action = reverse('initiatives:edit', kwargs={'pk': instance.id})
             if country:
-                all_forms = Assessment.objects.filter(Q(slug="init_registration") | Q(slug="init_exec")).filter(location_id=int(country))
+                all_forms = Assessment.objects.filter(slug="init_exec").filter(location_id=int(country))
             else:
                 all_forms = Assessment.objects.filter(Q(slug="init_registration") | Q(slug="init_exec"))
             # all_forms = Assessment.objects.get(Assessment.slug in('init_registration','init_exec', 'post_post_assessment'))
