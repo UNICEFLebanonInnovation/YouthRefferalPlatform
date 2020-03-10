866b0377140c0216f580cb69d9953f0f7e9a8ccd
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2020-03-10 10:46:22 +0200

    removed disabled

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -189,7 +189,7 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
 
                 if youth_registered:
                     if specific_form.slug == "init_registration":
-                        disabled = "disabled"
+                        disabled = ""
                     # check if the pre is already filled
                     else:
                         order = 1  # int(specific_form.order.split(".")[1])
@@ -198,19 +198,19 @@ class YouthLedInitiativePlanningForm(forms.ModelForm):
                             form_submitted = AssessmentSubmission.objects.filter(
                                 assessment_id=specific_form.id, initiative_id=instance.id).exists()
                             if form_submitted:
-                                disabled = "disabled"
+                                disabled = ""
                         else:
                             # make sure the user filled the form behind this one in order to enable it
                             if previous_status == "disabled":
                                 previous_submitted = AssessmentSubmission.objects.filter(
                                     assessment_id=specific_form.id, initiative_id=instance.id).exists()
                                 if previous_submitted:
-                                    disabled = "disabled"
+                                    disabled = ""
                             else:
-                                disabled = "disabled"
+                                disabled = ""
                 else:
                     if specific_form.slug != "init_registration":
-                        disabled = "disabled"
+                        disabled = ""
 
                 if specific_form.name not in new_forms:
                     new_forms[specific_form.name] = OrderedDict()
