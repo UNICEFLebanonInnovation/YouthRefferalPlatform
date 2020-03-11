33cb3a8cfc03d52a52a09f5927d8faff2a9c3b28
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2020-03-11 10:15:07 +0200

    added _t to participants

--- referral_platform/initiatives/form.py
+++ referral_platform/initiatives/form.py
@@ -2,7 +2,6 @@
 
 from __future__ import unicode_literals, absolute_import, division
 
-from django.utils.translation import ugettext as _
 from django import forms
 from django.core.urlresolvers import reverse
 from django.db.models import Q
@@ -25,7 +24,7 @@ YES_NO_CHOICE = ((False, _('No')), (True, _('Yes')))
 
 class YouthLedInitiativePlanningForm(forms.ModelForm):
     Participants = forms.ModelMultipleChoiceField(queryset=Registration.objects.none(),
-                                                  widget=FilteredSelectMultiple("Participants", is_stacked=False), label=_("Participants"),)
+                                                  widget=FilteredSelectMultiple("Participants", is_stacked=False), label=_t("Participants"),)
 
     partner_organization = forms.ModelChoiceField(
         label=_('Partner Organization'),
