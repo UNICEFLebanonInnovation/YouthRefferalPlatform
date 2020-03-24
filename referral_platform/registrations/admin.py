c7af14a401cd841c2c5405d3a68cca6ae79a878c
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2020-03-24 20:37:59 +0200

    -- to name

--- referral_platform/registrations/admin.py
+++ referral_platform/registrations/admin.py
@@ -386,14 +386,14 @@ class AssessmentHashResource(resources.ModelResource):
             'hashed',
             'registration',
             'assessment_slug',
-            'partner_name',
+            'partner__name',
 
 
         )
         search_fields = (
             'hashed',
             'assessment_slug',
-            'partner_name',
+            'partner__name',
 
         )
         export_order = fields
@@ -409,11 +409,11 @@ class AssessmentHashAdmin(ImportExportModelAdmin):
         'hashed',
         'registration',
         'assessment_slug',
-        'partner_name',
+        'partner__name',
     )
     list_filter = (
         'assessment_slug',
-        'partner_name',
+        'partner__name',
     )
 
     search_fields = (
