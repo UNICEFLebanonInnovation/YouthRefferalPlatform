f49d0adce4e04767d9bd11cb37081322d391d50d
Rami Sweidane <rsweidan@Ramis-MacBook-Pro.local>
2020-03-25 08:47:37 +0200

    partner

--- referral_platform/registrations/admin.py
+++ referral_platform/registrations/admin.py
@@ -409,7 +409,7 @@ class AssessmentHashAdmin(ImportExportModelAdmin):
         'hashed',
         'registration',
         'assessment_slug',
-        'partner__name',
+        'partner',
     )
     list_filter = (
         'assessment_slug',
